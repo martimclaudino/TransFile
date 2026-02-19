5e6c5806f258445a9a65202a0dfaa89217d59f0d
Martim <martimffclaudino@tecnico.ulisboa.pt>
2026-02-19 23:20:23 +0000

    Windows App UI layout

--- Windows/Views/MainWindow.xaml.cs
+++ Windows/Views/MainWindow.xaml.cs
@@ -1,23 +1,46 @@
-﻿using System.Text;
+﻿using Microsoft.Win32;
+using System.Diagnostics;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace Windows;
-
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
-public partial class MainWindow : Window
+namespace Windows.Views
 {
-    public MainWindow()
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseQr_Click(object sender, RoutedEventArgs e)
+        {
+            BtnGenerateQr.IsChecked = false;
+        }
+
+        private void AddFiles_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+            openFileDialog.Title = "Select files to share with your phone";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string[] selectedFiles = openFileDialog.FileNames;
+                foreach (string file in selectedFiles)
+                {
+                    Debug.WriteLine($"Ficheiro selecionado: {file}");
+                }
+            }
+        }
+
+        private void SelectPath_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFolderDialog openFolderDialog = new OpenFolderDialog();
+            openFolderDialog.Title = "Select default download folder";
+
+            if (openFolderDialog.ShowDialog() == true)
+            {
+                BtnDownloadPath.Content = openFolderDialog.FolderName;
+            }
+        }
     }
 }
